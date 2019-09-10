4dbd7b73cd153e3606ed89d050ba6ede80d67eb7
Peiqi Ji <842779063@qq.com>
2019-09-09 21:25:49 -0500

    lab1-finish

--- lab1/list.c
+++ lab1/list.c
@@ -47,22 +47,36 @@ void print_list(struct node* head){
 struct node* insert_list (struct node* head, int num){
   //TO DO
   // create a node, store num there, and insert the node in the list so that the list remains in sorted order
+  //create a node to store head
   struct node* tmp = head;
+  //a node to store num
   struct node * number =(struct node*)malloc(sizeof(struct node));
+  number->x=num;
+  number->next=NULL;
+  // a pointer always points head
   struct node * hd =(struct node*)malloc(sizeof(struct node));
   hd->x = 0;
   hd->next = tmp;
-  number->x=num;
-  while(tmp->next!=NULL){
-    if((tmp->next->x)<=num){
-      number->next=tmp->next;
-      tmp->next=number;
-      break;
+  //if num is smalles
+  if(tmp->x>num){
+    number->next=tmp;
+    hd->next = number;
+  }else{
+    //if num in middle
+    while(tmp->next!=NULL){
+      if((tmp->next->x)>=num){
+        number->next=tmp->next;
+        tmp->next=number;
+        break;
+      }
+      tmp=tmp->next;
+    }//if num is biggest
+    if(tmp->next==NULL){
+      tmp->next = number;
     }
-    tmp=tmp->next;
   }
-  print_list(hd);
-  return hd->next;
+  head = hd->next;
+  return head;
 }
 
 int main (int argc, char** argv){
