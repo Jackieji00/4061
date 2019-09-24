d38377cec28aaa7285de66237f07ebf9b087220d
Peiqi Ji <842779063@qq.com>
2019-09-24 16:40:01 -0500

    lab3 test

--- lab3/template-code.c
+++ lab3/template-code.c
@@ -11,25 +11,27 @@ int main(int argc, char** argv) {
 		printf("Incorrect number of args: given %d, expected 1.\n", argc - 1);
 		exit(1);
 	}
-	char n[] = argv[1];
 
 	pid_t child_pid1 = fork();
 	if(child_pid1 <0){
 		printf("can't fork, error occured\n");
 		exit(1);
 	}else if (child_pid1 ==0){
-		execl("/bin/bash","-c","echo \"hello world!\"",NULL);
-		exit(1);
+		printf("I am child_1!\n");
+		execl("/bin/echo","-c", "hello world!",NULL);
 	}else{
+		wait(NULL);
+		pid_t child_pid2 = fork();
+		char *arr[] = {argv[1],NULL};
+		if(child_pid2 <0){
+			printf("can't fork, error occured\n");
+			exit(1);
+		}else if (child_pid2 ==0){
+			printf("I am child_2!\n");
+			execv("./rtime.o",arr);
+		}else{
+			wait(NULL);
+		}
+	}
 
-	}
-	pid_t child_pid2 = fork();
-	const char *arr[] = {"-o",n,NULL}
-	if(child_pid2 <0){
-		printf("can't fork, error occured\n");
-		exit(1);
-	}else if (child_pid2 ==0){
-		execv("rtime.o",n);
-		exit(1);
-	}
 }
