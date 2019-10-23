d13c2194d927de674225b365cd22db6292e0e3c6
Dian Zhang <zhan5999@umn.edu>
2019-10-22 22:52:05 -0500

    draft

--- project/project2/include/phase3.h
+++ project/project2/include/phase3.h
@@ -2,3 +2,16 @@
 // Function prototype for 
 //			reduce function that will receive list from all the Mappers to create a single list
 //			the single list is then written to ReducerResult.txt
+#ifndef PHASE3_H
+#define PHASE3_H
+
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include "variables.h"
+
+#endif
