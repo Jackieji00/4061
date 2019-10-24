e1f045f4dd32dfaf02a1fa021b2842e912174811
Dian Zhang <zhan5999@umn.edu>
2019-10-23 19:30:28 -0500

    debugg

--- project/project2/src/phase4.c
+++ project/project2/src/phase4.c
@@ -5,6 +5,26 @@
 	2) 	Print the contents to standard output which is redirected to "FinalResult.txt"
 			i.e., on using printf, the statement should be written to "FinalResult.txt"
 */
+int finalResult(int *fd){
+//FILE * fd;
+    int * final;
+    final = malloc(ALPHA_NUM_SIZE*sizeof(val));
+    close(fd[1]);
+    read(fd[1],final,sizeof(final));
+    FILE *fptr;
+    //int fd2 = open("FinalResult.txt", O_WRONLY | O_CREAT);
+    fptr = freopen("FinalResult.txt", "a+", stdout);
+    //dup2(1, fd2);
+//newfd=dup2(fd, STDIN_FILENO);
+    return 0;
+}
 
-fd=open("ReducerResult.txt", "w+");
-newfd=dup2(fd, STDIN_FILENO);
+//int finalResult(int * fd){
+//    int * final;
+//    final = malloc(ALPHA_NUM_SIZE*sizeof(val));
+//    close(fd[1]);
+//    read(fd[0],final,sizeof(final));
+//    FILE * fptr;
+//    fptr = freopen("FinalResult.txt", "a+",stdout);
+//    return 0;
+//}
