869f3ae02629ccf18001274980fa1af31b5f9c8e
Dian Zhang <zhan5999@umn.edu>
2019-11-08 15:49:50 -0600

    initial

--- project/project3/main.c
+++ project/project3/main.c
@@ -8,4 +8,20 @@
 #include "header.h"
 // pthread.h included in header.h
 
-
+int main(int argc, char *argv[]){
+    if(argc<5){
+        fprintf(stderr," Need more argument\n");
+        return 1;
+    }
+    else if(argc == 5){
+        DIR *dr;
+        if((dr=opendir(argv[1]))==NULL){
+            fprintf(stderr, "%s is invalid\n",argv[1] );
+            return 2;
+        }
+    }
+    else{
+        fprintf(stderr,"Too much arguements\n");
+        return 3;
+    }
+}
