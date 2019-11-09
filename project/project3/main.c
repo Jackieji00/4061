920e1bba0e2b1c3d39a483c3feb589e447f1a222
Dian Zhang <zhan5999@umn.edu>
2019-11-09 00:07:07 -0600

    initial

--- project/project3/main.c
+++ project/project3/main.c
@@ -8,20 +8,47 @@
 #include "header.h"
 // pthread.h included in header.h
 
+struct thread_s{
+    char c;
+    struct thread_s *next;
+};
+
+//struct buffer {
+//    int vals[100];
+//    int index; //indicates the next available spot
+//};
+
+struct queue {
+     struct thread_s *head;
+     pthread_cond_t* cond;
+     pthread_mutex_t* mutex;
+};
+
 int main(int argc, char *argv[]){
-    if(argc<5){
+    if(argc<4){// 4 for no extra credits
         fprintf(stderr," Need more argument\n");
         return 1;
     }
-    else if(argc == 5){
+    else if(argc == 4){
         DIR *dr;
         if((dr=opendir(argv[1]))==NULL){
             fprintf(stderr, "%s is invalid\n",argv[1] );
             return 2;
+//            else{
+//                FILE * fp = fopen(argv[1],"r");//Read the file
+//                int lines;
+//            }
         }
     }
     else{
         fprintf(stderr,"Too much arguements\n");
         return 3;
     }
+
+
+    
+    
+    
+
+
 }
