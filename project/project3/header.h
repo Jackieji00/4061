890785fbab82290607650e51a6b6685f3a9198f8
Dian Zhang <zhan5999@umn.edu>
2019-11-09 00:06:58 -0600

    initial

--- project/project3/header.h
+++ project/project3/header.h
@@ -13,10 +13,21 @@ it will:
 #ifndef _HEADER_H_
 #define _HEADER_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+#include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/time.h>
+
 // header here..
-
-
-
 
 
 
