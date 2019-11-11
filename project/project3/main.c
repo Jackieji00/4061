8f6994dd1899fbb6d429bc0045dfbae06cd9f35b
user <user@karlens-mbp.lan>
2019-11-11 13:39:57 -0600

    a

--- project/project3/main.c
+++ project/project3/main.c
@@ -10,13 +10,12 @@ int end = FALSE;
 int alphaCount[25];
 char * option ="--";
 int main(int argc, char *argv[]){
-  if(argc<4){// 4 for no extra credits
+
+  if(argc<3){// 4 for no extra credits
     fprintf(stderr," Need more argument\n");
     return 1;
-  }
-  else if(argc==3||argc == 4){
+  }else if(argc==3||argc == 4){
     FILE *fp;
-
     if((fp = fopen(argv[2],"r"))== NULL){
       fprintf(stderr, "%s is invalid\n",argv[2] );
       return 2;
@@ -30,9 +29,11 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "%s is invalid\n",argv[3]);
         return 3;
       }
+
       int numCosumer = atoi(argv[1]);
       FILE logfile;
       // Create threads.
+
       pthread_t condPool[numCosumer+1];
       struct condBuffer* cq = (struct condBuffer*) malloc(sizeof(struct condBuffer));
     	cq->q = (struct buffer*) malloc(sizeof(struct buffer));
@@ -48,12 +49,14 @@ int main(int argc, char *argv[]){
 
       pthread_create(&condPool[0], NULL, producer, (void*) cq);
 
-    	for (int i=0; i < numCosumer; ++i) {
+    	for (int i=0; i < numCosumer; i++) {
         cq->consumerId=i;
     		pthread_create(&condPool[1 + i], NULL, consumer, (void*) cq); //start 50 consumer threads
     	}
-    	for (int i=0; i < 100; ++i) pthread_join(condPool[i], NULL); //wait for all the threads to be finished
-
+    	for (int i=0; i < numCosumer+1; i++){
+        printf("%s\n","here" );
+        pthread_join(condPool[i], NULL); //wait for all the threads to be finished
+      }
     }
     finilize();
   }
